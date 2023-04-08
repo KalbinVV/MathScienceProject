cc4e26ade133981b51d71fda7524cc8f0358990d
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-04-08 15:51:23 +0500

    Fixed bug with language file destination.

--- Configuration/Language/LanguageConfiguration.py
+++ Configuration/Language/LanguageConfiguration.py
@@ -4,7 +4,7 @@ from yaml.loader import SafeLoader
 
 class LanguageConfiguration:
     def __init__(self, file_name):
-        with open(f'Configuration/Language/{file_name}', 'r') as file:
+        with open(f'./Configuration/Language/{file_name}', 'r') as file:
             self.__data = yaml.load(file, Loader=SafeLoader)
 
     def __getitem__(self, item):
