69d0f359801381f789624adc2f12b1aab0527a84
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-04-21 00:12:54 +0500

    Code refactoring.

--- web/static/js/utils/modals.js
+++ web/static/js/utils/modals.js
@@ -1,37 +1,20 @@
-// Singleton function for modal
 function initModal(){
-    let modal = null
+    // Selectors of certain elements
+    const domElement = {
+        body: document.querySelector('.popup'),
+        title: document.querySelector('.popup_title'),
+        content: document.querySelector('.popup_content'),
+        cover: document.querySelector('.cover')
+    }
 
-    const getModal = () => {
-        if(modal != null) {
-            return modal
-        }
-
-        // Selectors of certain elements
-        const domElement = {
-            body: document.querySelector('.popup'),
-            title: document.querySelector('.popup_title'),
-            content: document.querySelector('.popup_content'),
-            cover: document.querySelector('.cover')
-        }
-
-        const show = () => {
+    return {
+        show: () => {
             domElement.body.style.display = 'block'
             domElement.cover.style.display = 'block'
-        }
-
-        const setAttributes = (title, content) => {
+        },
+        setAttributes: (title, content) => {
             domElement.title.innerHTML = title
             domElement.content.innerHTML = content
         }
-
-        modal = {
-            show: show,
-            setAttributes: setAttributes
-        }
-
-        return modal
     }
-
-    return getModal()
 }
